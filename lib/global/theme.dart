7c815a94b1800171f0fca21fde810bfe4ee1d542
Dk-variya007 <variyadivyesh79@gmail.com>
2024-12-14 09:14:55 +0530

    background add

--- lib/global/theme.dart
+++ lib/global/theme.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 abstract class AppThemes {
   static final ThemeData lightTheme = ThemeData(
-    brightness: Brightness.light,
-    fontFamily: 'Inter',
-    useMaterial3: true,
-  );
+      brightness: Brightness.light,
+      fontFamily: 'Inter',
+      useMaterial3: true,
+      scaffoldBackgroundColor: Colors.white);
 }
