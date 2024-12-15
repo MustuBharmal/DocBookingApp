e2de4ac599873a2802b369dedf5fe83775c5bba0
murtaza786 <murtazatinwala1511@gmail.com>
2024-12-15 11:46:16 +0530

    Contact Us Screen and How to Be Partner Screen

--- lib/presentations/profile/view/contact_us_screen.dart
+++ lib/presentations/profile/view/contact_us_screen.dart
@@ -1,19 +1,88 @@
 import 'package:doc_booking_app/presentations/profile/controller/profile_controller.dart';
+import 'package:doc_booking_app/presentations/profile/widgets/custom_drop_down.dart';
+import 'package:doc_booking_app/presentations/profile/widgets/custom_phone_field.dart';
+import 'package:doc_booking_app/presentations/profile/widgets/custom_text_field.dart';
+import 'package:doc_booking_app/widgets/blue_button.dart';
+import 'package:doc_booking_app/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../global/styles.dart';
+
 class ContactUsScreen extends GetView<ProfileController> {
   static const String routeName = '/contact-us-screen';
+
   const ContactUsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-body: Column(
-  children: [
-    // CustomTextField(label: 'Full Name', showAsterisk: true, controller: controller)
-  ],
-),
+      appBar: CustomAppBar(
+        title: 'Contact Us',
+        back: true,
+        isVisible: false,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  'Let us know about your query',
+                  style: txtInterTextField,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: CustomTextField(
+                    label: 'Full Name',
+                    showAsterisk: true,
+                    controller: controller.nameController,
+                    hintStyle: txtInterTextFieldHint,
+                    hintText: 'John Doe'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: CustomTextField(
+                    label: 'Email Address',
+                    showAsterisk: true,
+                    controller: controller.emailController,
+                    hintStyle: txtInterTextFieldHint,
+                    hintText: 'john.doe@gmail.com'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: CustomPhoneField(controller: controller.phoneController),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: CustomDropdown(
+                  label: 'Preferred Communication Method',
+                  showAsterisk: true,
+                  selectedItem: controller.prefCommMethod.value,
+                  items: controller.prefCommMethodList,
+                  onChanged: (prefCommMethod) {
+                    controller.prefCommMethod.value = prefCommMethod!;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0,bottom: 20),
+                child: CustomTextField(
+                    label: 'Message',
+                    showAsterisk: true,
+                    controller: controller.messageController,
+                    hintStyle: txtInterTextFieldHint,
+                    hintText: 'Type your Message here'),
+              ),
+              BlueButton(label: 'Submit'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
