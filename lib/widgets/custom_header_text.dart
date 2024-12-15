3c724182c47cb39ff10e3cfb9729441c8894fe6a
MustuBharmal <mustafabharmal10@gmail.com>
2024-12-15 12:41:01 +0530

    warning removal

--- lib/widgets/custom_header_text.dart
+++ lib/widgets/custom_header_text.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../global/styles.dart';
 
-import '../global/styles.dart';
-
-import '../global/styles.dart';
 
 class CustomerHeaderText extends StatelessWidget {
   const CustomerHeaderText({super.key, required this.text, this.button});
