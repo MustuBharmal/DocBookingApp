9a34809b5aeaf26dd22c9880d4232c27bef36992
Dk-variya007 <variyadivyesh79@gmail.com>
2024-12-16 14:37:49 +0530

    app bar change

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -6,8 +6,14 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final bool back;
   final bool isVisible;
+  final VoidCallback? onPressed;
 
-  const CustomAppBar({required this.title, this.isVisible = true, required this.back, super.key});
+  const CustomAppBar(
+      {required this.title,
+      this.onPressed,
+      this.isVisible = true,
+      required this.back,
+      super.key});
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
@@ -17,11 +23,32 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     return Padding(
       padding: const EdgeInsets.only(top: 22.0),
       child: AppBar(
-        scrolledUnderElevation: 0,
         title: Text(title),
         centerTitle: true,
-        actions: [Visibility(visible: isVisible, child: IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)))],
-        backgroundColor: AppColors.transparent,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Visibility(
+                visible: isVisible,
+                child: InkWell(
+                  onTap: onPressed,
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Color(0xFFF5F1FD))),
+                    child: Icon(
+                      Icons.notifications,
+                      size: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                )),
+          )
+        ],
+        backgroundColor: AppColors.white,
         iconTheme: IconThemeData(color: AppColors.black),
       ),
     );
