7e39c2497646d42e6d7d39283de34febe2a29401
murtaza786 <murtazatinwala1511@gmail.com>
2024-12-20 18:55:16 +0530

    specialist model

---
+++ lib/presentations/specialist/models/specialist.dart
@@ -0,0 +1,23 @@
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'specialist.freezed.dart';
+
+part 'specialist.g.dart';
+
+@freezed
+class Specialist with _$Specialist {
+  const factory Specialist({
+    String? icon,
+    int? id,
+    String? name,
+    @JsonKey(name: 'is_active') bool? isActive,
+    String? createdAt,
+    String? updatedAt
+
+  }) = _Specialist;
+
+  factory Specialist.fromJson(Map<String, Object?> json)
+  => _$SpecialistFromJson(json);
+}
