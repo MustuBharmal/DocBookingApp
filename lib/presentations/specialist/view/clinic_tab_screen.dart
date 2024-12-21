f938392b501c78568e504c394eeb34a8931c60a6
Dk-variya007 <variyadivyesh79@gmail.com>
2024-12-21 14:58:55 +0530

    Tab bar add

---
+++ lib/presentations/specialist/view/clinic_tab_screen.dart
@@ -0,0 +1,35 @@
+import 'package:doc_booking_app/presentations/specialist/view/specialist_detail_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../widgets/custom_specialist_container.dart';
+import '../../home/controller/home_controller.dart';
+
+class ClinicTabScreen extends StatelessWidget {
+  const ClinicTabScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      return ListView.separated(
+        itemBuilder: (context, index) {
+          final doctorList = HomeController.instance.doctorList[index];
+          return CustomSpecialistContainer(
+            picPath: doctorList?.profilePic ?? '',
+            name: doctorList?.name ?? '',
+            specialist: doctorList?.specialization ?? '',
+            charges: doctorList?.fees ?? '',
+            onPressed: () {
+              Get.to(SpecialistDetailScreen(
+                doctor: doctorList!,
+              ));
+            },
+          );
+        },
+        separatorBuilder: (context, index) => const SizedBox(height: 10),
+        itemCount: HomeController.instance.doctorList.length,
+      );
+    });
+  }
+}
+
--- lib/presentations/specialist/view/list_of_specialist_screen.dart
+++ lib/presentations/specialist/view/list_of_specialist_screen.dart
@@ -1,14 +1,13 @@
 import 'package:doc_booking_app/global/constant_string.dart';
-import 'package:doc_booking_app/presentations/home/controller/home_controller.dart';
 import 'package:doc_booking_app/presentations/specialist/controller/specialist_controller.dart';
-import 'package:doc_booking_app/presentations/specialist/view/specialist_detail_screen.dart';
+import 'package:doc_booking_app/presentations/specialist/view/clinic_tab_screen.dart';
+import 'package:doc_booking_app/presentations/specialist/view/home_tab_screen.dart';
 import 'package:doc_booking_app/presentations/specialist/widget/custom_search_textfield.dart';
 import 'package:doc_booking_app/widgets/custom_app_bar.dart';
-import 'package:doc_booking_app/widgets/custom_container_with_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../global/images.dart';
-import '../../../widgets/custom_specialist_container.dart';
+import '../../../widgets/custom_tab_bar.dart';
 import '../widget/custom_button.dart';
 
 class ListOfSpecialistScreen extends GetView<SpecialistController> {
@@ -18,79 +17,53 @@ class ListOfSpecialistScreen extends GetView<SpecialistController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
         appBar: const CustomAppBar(title: 'Search', back: true),
-        body: Stack(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16),
-              child: SingleChildScrollView(
-                child: Column(
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Obx(
+                  () => CustomSearchTextfield(
+                    hintText: ConstantString.searchByName,
+                    controller:
+                        SpecialistController.instance.searchController.value,
+                  ),
+                ),
+              ),
+              const CustomTabBar(
+                tabText1: 'Home',
+                tabText2: 'Clinic',
+              ),
+              Expanded(
+                child: TabBarView(
                   children: [
-                    Obx(
-                      () => CustomSearchTextfield(
-                        hintText: ConstantString.searchByName,
-                        controller: SpecialistController
-                            .instance.searchController.value,
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          CustomContainerWithText(
-                            text: ConstantString.home,
-                          ),
-                          CustomContainerWithText(
-                            text: ConstantString.clinic,
-                          )
-                        ],
-                      ),
-                    ),
-                    Obx(() {
-                      return ListView.separated(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        primary: true,
-                        itemBuilder: (context, index) {
-                          final doctorList =
-                              HomeController.instance.doctorList[index];
-                          return CustomSpecialistContainer(
-                            picPath: doctorList?.profilePic ?? '',
-                            name: doctorList?.name ?? '',
-                            specialist: doctorList?.specialization ?? '',
-                            charges: doctorList?.fees ?? '',
-                            // rating: specialist.rating,
-                            // review: specialist.review,
-                            onPressed: () {
-                              Get.to(SpecialistDetailScreen(doctor: doctorList!,));
-                            },
-                          );
-                        },
-                        separatorBuilder: (context, index) =>
-                            const SizedBox(height: 10),
-                        itemCount: HomeController.instance.doctorList.length,
-                      );
-                    }),
+                    HomeTabScreen(),
+                    ClinicTabScreen(),
                   ],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 23, right: 16),
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: CustomButton(
-                  onPressed: () {},
-                  height: Get.height * 0.05,
-                  width: Get.width * 0.33,
-                  iconPath: AppImage.map,
-                  label: ConstantString.mapView,
+              Padding(
+                padding: const EdgeInsets.only(bottom: 23, right: 16),
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: CustomButton(
+                    onPressed: () {},
+                    height: Get.height * 0.05,
+                    width: Get.width * 0.33,
+                    iconPath: AppImage.map,
+                    label: ConstantString.mapView,
+                  ),
                 ),
               ),
-            )
-          ],
-        ));
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
